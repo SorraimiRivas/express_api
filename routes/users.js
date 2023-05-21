248da66e41e040c1a54ca5f410a276f96953a0fb
Sorraimi Rivas <smorel@applaudostudios.dev>
2023-05-21 01:01:45 -0400

    added user routes for get, delete and patch with id identifier

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,47 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: 30,
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 25,
-  },
-];
+let users = [];
 
 router.get("/", (req, res) => {
   res.send(users);
-  console.log(users);
 });
 
 router.post("/", (req, res) => {
   const user = req.body;
-  users.push(user);
+  users.push({ ...user, id: uuidv4() });
+
   res.send(`User with the name ${user.firstName} added to the DB`);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.id === id);
+
+  res.send(foundUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  users = users.filter((user) => user.id !== id);
+
+  res.send(`User with id ${id} has been removed from the DB`);
+});
+
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, age } = req.body;
+  const user = users.find((user) => user.id === id);
+
+  if (firstName) user.firstName = firstName;
+  if (lastName) user.lastName = lastName;
+  if (age) user.age = age;
+
+  res.send(`User with id ${id} has been updated`);
 });
 
 export default router;
